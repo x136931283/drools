81be71b0412a3093626f79af98306de46775d9ab
tian <136931283@qq.com>
2022-02-13 00:08:03 +0800

    droolsDemo

--- spring-boot-drools/src/main/java/com/tianblogs/drools/controller/HelloController.java
+++ spring-boot-drools/src/main/java/com/tianblogs/drools/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @Autowired
     private RuleService ruleService;
-    @RequestMapping("/rules")
+    @RequestMapping("/rule")
     public String rule(){
         ruleService.rule();
         return "OK";
